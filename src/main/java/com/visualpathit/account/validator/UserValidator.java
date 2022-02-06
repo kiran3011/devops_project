ac841249b5defa2842590f79295ebc9d84c62cf0
awsdevops009 <47270229+awsdevops009@users.noreply.github.com>
2022-02-06 23:45:59 +0530

    Update UserValidator.java

--- src/main/java/com/visualpathit/account/validator/UserValidator.java
+++ src/main/java/com/visualpathit/account/validator/UserValidator.java
@@ -11,6 +11,7 @@ import org.springframework.validation.Validator;
 
 @Component
 public class UserValidator implements Validator {
+    System.out.println("Hello Java");
     @Autowired
     private UserService userService;
 
